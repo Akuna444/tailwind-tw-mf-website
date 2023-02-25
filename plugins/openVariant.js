80828995ead3dba283be4e29f3ef2922cde578c0
Akuna444 <aklilsolomon3@gmail.com>
2023-02-25 11:08:23 +0300

    add mobile menu holder

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -2,6 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 const openVariant = plugin(function ({ addVariant }) {
   addVariant("group-open", ":merge(.group).open &");
+  addVariant("peer-open", ":merge(.peer).open ~ &");
 });
 
 module.exports = openVariant;
